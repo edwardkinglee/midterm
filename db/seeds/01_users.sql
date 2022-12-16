d5ef347c9e662e5987fa1d1a314ab1045e95550b
edwardkinglee <edward.lee3@telus.com>
2022-12-16 17:23:06 -0500

    Create tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password) VALUES ('Alice', 'alice@email.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('Kira', 'kira@email.com', 'password');
