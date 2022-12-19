b5cfac6101ee7f1c0d7331648df4267b97a0795c
edwardkinglee <edward.lee3@telus.com>
2022-12-19 16:02:16 -0500

    Update features template to make listings

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,17 +13,20 @@ $().ready(function() {
         for (const listing of response.listings) {
 
           let $listing = `
-            <div class="col-md-4">
-              <div class="thumbnail">
-                <a href="/listings/${listing.id}">
-                <img src="${listing.photo}" style="width:100%">
-                  <div class="caption">
-                    <h3>${listing.year} ${listing.make} ${listing.model}</h3>
-                    <p>${listing.description}</p>
-                  </div>
-                </a>
-              </div>
-            </div>`;
+          <div class="col">
+          <div class="card">
+            <img src="${listing.photo}" class="card-img-top" alt="...">
+            <h5 class="card-header">${listing.year} ${listing.make} ${listing.model}</h5>
+            <div class="card-body">
+              <h5 class="card-title">${listing.price} ${listing.color}</h5>
+              <p class="card-text">${listing.description}</p>
+              <a href="/listings/${listing.id}" class="btn btn-primary">View listing</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted: ${listing.timestamp}
+            </div>
+          </div>
+        </div>`;
 
           $($listing).appendTo($featuredContainer);
 
