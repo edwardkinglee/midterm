0303d28fa1281462600fd3ce8a27c5970751f804
edwardkinglee <edward.lee3@telus.com>
2022-12-22 17:16:42 -0500

    Change search

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 $().ready(function() {
 
   $.ajax({
--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { reset } = require('nodemon');
 const router = express.Router();
 
 // All listings
 router.get('/', (req, res) => {
   const userId = req.cookies.user_id;
-  console.log('response', res);
+ 
   const templateVars = { display: 'All', userId };
 
   res.render('search', templateVars);
@@ -12,9 +13,11 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  const userId = req.cookies.user_id;
   const searchOptions = req.body;
-  console.log(searchOptions);
-
+  const templateVars = { display: 'All', userId, searchOptions };
+  console.log('searchOptions templateVars', templateVars);
+  res.render('search', templateVars);
 });
 
 module.exports = router;
