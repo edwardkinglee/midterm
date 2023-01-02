a5db5e85ad74bb816f59ea3b54b89f1fba3baf58
edwardkinglee <edward.lee3@telus.com>
2023-01-02 05:22:47 -0500

    Update index.js price format

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -19,8 +19,8 @@ $().ready(function() {
             <img src="${listing.photo}" class="card-img-top" alt="...">
             <h5 class="card-header">${listing.year} ${listing.make} ${listing.model}</h5>
             <div class="card-body">
-              <h5 class="card-title">${listing.price} ${listing.color}</h5>
-              <p class="card-text">${listing.description}</p>
+              <h5 class="card-title">$${Number(listing.price).toLocaleString('en')}</h5>
+              <p class="card-text">${listing.description}<br>color: ${listing.color} <br> ${listing.body_type}</p>
               <a href="/listings/${listing.id}" class="btn btn-primary stretched-link">View listing</a>
             </div>
             <div class="card-footer text-muted">
