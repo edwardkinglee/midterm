a582e5034febf0ea47f1c7ca872eccee0a8eed66
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-02 11:27:04 -0700

    updated favourites page to render listings as rows

--- public/scripts/favourites.js
+++ public/scripts/favourites.js
@@ -18,20 +18,35 @@ $().ready(function() {
       for (const favourite of response.Favourites) {
 
         let $favourite = `
-        <div class="col">
-          <div class="card">
-            <img src="${favourite.photo}" class="card-img-top" alt="...">
-            <h5 class="card-header">${favourite.year} ${favourite.make} ${favourite.model}</h5>
-            <div class="card-body">
-              <h5 class="card-title">${favourite.price} ${favourite.color}</h5>
-              <p class="card-text">${favourite.description}</p>
-              <a href="/listings/${favourite.id}" class="btn btn-primary">View listing</a>
+        <div class="card-body">
+            <div class="row border-top border-bottom">
+              <div class="col-sm-2">
+               <a href="/listings/${favourite.id}" class="btn">
+                <img src="${favourite.photo}" class="img-fluid img-thumbnail" alt="..."/>
+               </a>
+              </div>
+
+              <div class="col-sm-5">
+                <div class="card-body-right text-start">
+                <a href="/listings/${favourite.id}" class="">
+                <h5 class="card-title">${favourite.year} ${favourite.make} ${favourite.model}</h5>
+                </a>
+                <br>
+                <p class="card-text">${favourite.description} <br>color: ${favourite.color} <br>${favourite.body_type}</p>
+                </div>
+              </div>
+
+              <div class="col-sm-2">
+              <h6>${Number(favourite.kms).toLocaleString('en')} kms</h6>
+              </div>
+
+              <div class="col-sm-3">
+              <h5>$${Number(favourite.price).toLocaleString('en')}</h5>
+              <p><br><br>Posted: ${new Date(favourite.timestamp).toDateString()}<br>${new Date(favourite.timestamp).toLocaleTimeString('en-US')}</p>
+              </div
             </div>
-            <div class="card-footer text-muted">
-              Posted: ${favourite.timestamp}
-            </div>
+
           </div>
-        </div>
         `;
 
         $($favourite).appendTo($favouritesContainer);
