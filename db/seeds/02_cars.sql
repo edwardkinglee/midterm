1318aa1aa317e7b5d1b92c16168f95b770d00632
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-04 13:09:19 -0700

    fix car seeds as car 10 should be owned by user 8

--- db/seeds/02_cars.sql
+++ db/seeds/02_cars.sql
@@ -8,16 +8,16 @@ INSERT INTO cars (lister_id, year, make, model, color, description, price, photo
         (5, 2023, 'Cadillac', 'Lyriq', 'Grey', 'electric!', 77000, 'https://cdn.motor1.com/images/mgl/6ZpKv7/s3/cadillac-lyriq.jpg', '2020-04-02T14:32:55.000Z', 'wagon', 15),
         (7, 2018, 'Mercedes-Benz', 'G-Class', 'Black', 'boxy', 125000, 'https://i.pinimg.com/originals/a5/2f/6e/a52f6ea1922573332a21a1d4e1d03481.jpg', '2020-07-02T04:04:55.000Z', 'suv', 17000),
         (8, 2025, 'Tesla', 'Cybertruck', 'Other', 'where did I even buy this?', 125000, 'https://electrek.co/wp-content/uploads/sites/3/2022/06/Tesla-Cybertruck-Moss-Landing-KSBW-Jerrett-Knapp-1.jpg?quality=82&strip=all&w=1600', '2020-01-23T05:30:55.000Z', 'truck', 0),
-        (7, 2017, 'Mercedes Benz', 'G550 Squared', 'White', 'Automatic, 8-Cylinder, AWD', 298000, 'https://img.jamesedition.com/listing_images/2019/01/08/19/17/45/ad6bdf7d-4001-4743-8f4c-449aef19004f/je/1100xxs.jpg', '2020-02-27T05:30:55.000Z', 'suv', 4000),
+        (8, 2017, 'Mercedes Benz', 'G550 Squared', 'White', 'Automatic, 8-Cylinder, AWD', 298000, 'https://img.jamesedition.com/listing_images/2019/01/08/19/17/45/ad6bdf7d-4001-4743-8f4c-449aef19004f/je/1100xxs.jpg', '2020-02-27T05:30:55.000Z', 'suv', 4000),
         (8, 2019, 'Ferrari', 'GTC4 Lusso', 'Red', 'Automatic, 8-Cylinder, Rwd', 240000, 'https://d2blhp03rkvfi4.cloudfront.net/cache/width_916/car/car-18546/photo/2019-Ferrari-GTC4Lusso-249988-1560814567.jpg', '2020-02-27T05:30:55.000Z', 'hatchback', 8000),
         (2, 1965, 'Citroen', 'F-Van', 'Silver', 'Manual, 4-Cylinder, Awd', 67900, 'https://img.jamesedition.com/listing_images/2019/12/13/08/16/08/596dfb58-e142-42c4-b11b-b85b31e7a4e2/je/1900xxs.jpg', '2020-02-27T05:30:55.000Z', 'minivan', 54000),
         (8, 2019, 'Jaguar', 'F-Type R', 'White', 'beep beep', 125000, 'https://cdn.carbuzz.com/gallery-images/840x560/639000/200/639225.jpg', '2020-02-27T05:30:55.000Z', 'coupe', 14000);
 
 
 -- user: 1 => listed car: 1
--- user: 2 => listed car: 2
+-- user: 2 => listed car: 2, 12
 -- user: 3 => listed car: 3, 4, 5
 -- user: 4 => listed car: 6
 -- user: 5 => listed car: 7
 -- user: 7 => listed car: 8
--- user: 8 => listed car: 9, 10
+-- user: 8 => listed car: 9, 10, 11, 13
