e91d2d162e6aafb0c96fe3d39fa9a6ea680f7adb
edwardkinglee <edward.lee3@telus.com>
2023-01-04 21:26:52 -0500

    Add heart icon to search page

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -36,7 +36,15 @@ $().ready(function() {
             </div>
              
             <div class="col-sm-3">
-             <h5>$${Number(car.price).toLocaleString('en')}</h5>
+             <h5 class="d-flex justify-content-between">
+             <div></div>
+             <div>
+                $${Number(car.price).toLocaleString('en')}
+             </div>
+             <div>
+             <i id="favorite-icon" class="fa-regular fa-heart"></i>
+             </div>
+             </h5>
              <p><br><br><br>Posted: ${new Date(car.timestamp).toLocaleDateString()} ${new Date(car.timestamp).toLocaleTimeString([], { timeStyle: 'short' })}</p>
              
             </div
