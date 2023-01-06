ab19373f3c88ffaa1917f04313bba69a048ad94e
edwardkinglee <edward.lee3@telus.com>
2023-01-05 20:46:58 -0500

    Add to show search options choosen

--- routes/search.js
+++ routes/search.js
@@ -2,12 +2,79 @@ const express = require('express');
 const { reset } = require('nodemon');
 const router = express.Router();
 
-// All listings
+// get search details
 router.get('/', (req, res) => {
   const userId = req.cookies.user_id;
   const searchQuery = req.query;
-  console.log('search query',searchQuery); 
-  const templateVars = { display: 'All', userId, searchQuery};
+  let make = req.query.make;
+  let minYear = req.query.min_year;
+  let maxYear = req.query.max_year;
+  let minKms = req.query.min_kms;
+  let maxKms = req.query.max_kms;
+  let minPrice = req.query.min_price;
+  let maxPrice = req.query.max_price;
+  let sort = req.query.sort;
+
+  if(!make){
+     make = 'Any Make'; 
+  }
+  
+  if(!minYear){
+    minYear = 'Any';
+ }
+
+ if(!maxYear){
+   maxYear = 'Any';
+ }
+  
+  if(!minKms){
+    minKms = 'Any kms';
+  }
+
+  if(minKms && !isNaN(minKms)){
+    minKms= Number(minKms).toLocaleString('en');
+  }
+  
+  if(!maxKms){
+    maxKms = 'Any kms';
+  }
+
+  if(maxKms && !isNaN(maxKms)){
+    maxKms= Number(maxKms).toLocaleString('en');
+  }
+
+  if(!minPrice){
+    minPrice = '$0';
+  }
+
+  if(minPrice && !isNaN(minPrice)){
+    minPrice = Number(minPrice).toLocaleString('en');
+  }
+
+  if(!maxPrice){
+    maxPrice = 'Unlimited';
+  }
+
+  if(maxPrice && !isNaN(maxPrice)){
+    maxPrice = Number(maxPrice).toLocaleString('en');
+  }
+
+  if(!sort){
+    sort = 'By Make A-Z';
+  }
+  
+  const searchOptions = {
+    make,
+    minYear,
+    maxYear,
+    minKms,
+    maxKms,
+    minPrice,
+    maxPrice,
+    sort
+ };
+
+  const templateVars = { display: 'All', userId, searchOptions};
   
   res.render('search', templateVars);
 
