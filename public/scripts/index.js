6c93060dbde7eae87ff4be5db51473990ae3871d
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-06 09:35:40 -0700

    added fav functionality to featured cars

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,21 +7,28 @@ $().ready(function() {
   })
     .done((response) => {
 
+      const userId = Number($('#user-id')[0].innerText);
       const $featuredContainer = $('#featured-cars');
 
       $featuredContainer.empty();
 
       for (const listing of response.listings) {
 
+        let favHeart = `<i onclick="addFav('${listing.id}')" class="fa-regular fa-heart favorite-icon"></i>`;
+
+        if (!userId) {
+          favHeart = '';
+        }
+
         let $listing = `
           <div class="col">
-          <div class="card">
+          <div class="card" id="${listing.id}">
           <a href="/listings/${listing.id}">
             <img src="${listing.photo}" class="card-img-top" alt="...">
           </a>
             <h5 class="card-header">${listing.year} ${listing.make} ${listing.model}</h5>
             <div class="card-body">
-              <h5 class="card-title d-flex justify-content-between">$${Number(listing.price).toLocaleString('en')} <i class="fa-regular fa-heart favorite-icon"></i></h5>
+              <h5 class="card-title d-flex justify-content-between">$${Number(listing.price).toLocaleString('en')} <div id="fav-heart">${favHeart}</div></h5>
               <p class="card-text">${listing.description}<br>color: ${listing.color} <br> ${listing.body_type}</p>
 
               <a href="/listings/${listing.id}" class="btn btn-primary">View listing</a>
@@ -35,7 +42,54 @@ $().ready(function() {
         $($listing).appendTo($featuredContainer);
 
       }
+    })
+    .then(() => {
+      const userId = Number($('#user-id')[0].innerText);
+      $.ajax({
+        method: 'GET',
+        url: `/api/favourites/user/${userId}`
+      })
+        .then((response) => {
+          for (const fav of response.Favourites) {
+            $(`#${fav.car_id}`).find('#fav-heart').html(`<i onclick="removeFav('${fav.car_id}')" class="fa-solid fa-heart favorite-icon"></i>`);
+          }
+        });
     });
 
 });
 
+const addFav = function(listingId) {
+
+  const userId = Number($('#user-id')[0].innerText);
+
+  $.ajax({
+    method: 'POST',
+    url: `/favourites`,
+    data: {
+      'user': userId,
+      'car': listingId
+    }
+  })
+    .done((response) => {
+      location.reload();
+    });
+
+};
+
+const removeFav = function(listingId) {
+
+  const userId = Number($('#user-id')[0].innerText);
+
+  $.ajax({
+    method: 'DELETE',
+    url: `/favourites`,
+    data: {
+      'user': userId,
+      'car': listingId
+    }
+  })
+    .done((response) => {
+      location.reload();
+    });
+
+};
